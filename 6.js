45a70bf1ed5625c4b8c524483b173d2a6c08db92
Vaishnavi <122547011+Vaishnavi-kota@users.noreply.github.com>
2023-06-05 18:31:31 +0530

    Add files via upload

---
+++ 6.js
@@ -0,0 +1,14 @@
+
+function f1(){
+   var arr;
+   var sum=0;
+    for(arr=0;arr<20;arr++){
+        console.log(arr);
+          if(arr%2==0) {
+    sum=sum+arr;
+      }
+    }
+ return sum;   
+}
+f1();
+   
